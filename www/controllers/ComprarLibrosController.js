a2a985f680c644d67ac691b8c6f98d3aae7e198d
Eduardo Guerra Bobadilla <eguerra@cafica.com.mx>
2017-10-30 19:57:28 -0700

    buscar libors

--- www/controllers/ComprarLibrosController.js
+++ www/controllers/ComprarLibrosController.js
@@ -10,12 +10,41 @@ app.controller('ComprarLibrosController', function($scope,$ionicPopup,$state,$io
                 template: '<ion-spinner icon="spiral"></ion-spinner><br>Monstrando Libros',
                 //duration :20000//Optional
             });
-
-            Libros.query({categoria:$stateParams.IdCategoria}, function(respuesta) {
+            console.log($stateParams.IdCategoria);
+            Libros.query({search:'getXcategoria',categoria:$stateParams.IdCategoria}, function(respuesta) {
             
                     $ionicLoading.hide();   
                     $scope.libros=respuesta.data;
             }, function(error) {
+                $ionicLoading.hide(); 
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Error',
+                    template: error.headers("Error")
+                    
+                });
+                
+            });
+        
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    $scope.BuscarLibros = function() {
+        try{
+            
+            $ionicLoading.show({
+                noBackdrop :false,
+                template: '<ion-spinner icon="spiral"></ion-spinner><br>Monstrando Libros',
+                //duration :20000//Optional
+            });
+            console.log("libro a buscar "+$stateParams.LibroaBuscar);
+            Libros.query({search:'getXlike',librolike:$stateParams.LibroaBuscar}, function(respuesta) {
+            
+                    $ionicLoading.hide();   
+                    $scope.libros=respuesta.data;
+            }, function(error) {
+                $ionicLoading.hide(); 
                 var alertPopup = $ionicPopup.alert({
                     title: 'Error',
                     template: error.headers("Error")
--- www/models/libros.js
+++ www/models/libros.js
@@ -1,10 +1,10 @@
 app.factory('Libros', function ($resource,resourceInterceptor) {
-		return $resource('http://200.52.220.238:8080/WSLibros.svc/libros/:item',
+		return $resource('http://localhost:8080/WSLibros.svc/libros/:item',
 											{item: "@item"},
 											{
 												'get':    {method:'GET',interceptor: resourceInterceptor},
 												'save':   {method:'POST',interceptor: resourceInterceptor},
-												'query':  {method:'GET', isArray:true,interceptor: resourceInterceptor},
+												'query':  {method:'GET', isArray:true,interceptor: resourceInterceptor,url:'http://localhost:8080/WSLibros.svc/libros/?searchBy=:search',params:"@search"},
 												'remove': {method:'DELETE',interceptor: resourceInterceptor},
 												'delete': {method:'DELETE',interceptor: resourceInterceptor}
 											}
