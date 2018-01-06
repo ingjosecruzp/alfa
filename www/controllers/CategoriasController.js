7a17281f84d0569f580bbce3fb655f6cb2f3e751
eduardo <eguerra.bobadilla@gmail.com>
2018-01-05 19:14:21 -0600

    eduardo estilos

--- www/controllers/CategoriasController.js
+++ www/controllers/CategoriasController.js
@@ -11,12 +11,7 @@ app.controller('CategoriasController', function($scope,$ionicPopup,$state,$ionic
           {'id':5,'name':'Programacion','img':'323232323'},
           {'id':1,'name':'Anatomina','img':'323232323'},
           {'id':2,'name':'Diseño','img':'323232323'},
-          {'id':3,'name':'Arte','img':'323232323'},
-          {'id':4,'name':'Fisioterapia','img':'323232323'},
-          {'id':1,'name':'Anatomina','img':'323232323'},
-          {'id':2,'name':'Diseño','img':'323232323'},
-          {'id':3,'name':'Arte','img':'323232323'},
-          {'id':4,'name':'Fisioterapia','img':'323232323'}
+     
       ];
       
       $scope.BtnBuscar= function(){
